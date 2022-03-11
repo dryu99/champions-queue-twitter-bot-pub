90726c26215ccdb47f1ec0587f72d78ab7f0b16a
Daniel Ryu <rujjiryu99@gmail.com>
2022-03-11 01:12:59 -0800

    edit pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  apps : [{
-    name   : "cq-stream-bot",
-    script : "yarn build && yarn start"
-  }]
-}
+  apps: [
+    {
+      name: "cq-stream-bot",
+      script: "yarn build && yarn start",
+      stop_exit_codes: [0], // this doesn't work lol https://github.com/Unitech/pm2/issues/5208
+      exp_backoff_restart_delay: 100,
+
+      // TODO we would use cron if stop_exit_codes worked (we don't want to turn auto_restart off)
+      // restart server every day at 10am and 6pm
+      // handle server exit for mondays vs other days in app logic
+      // cron_restart: '0 10,18 * * *'
+    },
+  ],
+};
