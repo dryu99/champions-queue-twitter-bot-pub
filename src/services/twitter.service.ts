4573892847a36192bf330fd620dae032c0a769fc
Daniel Ryu <rujjiryu99@gmail.com>
2022-03-14 14:03:45 -0700

    ignore tv emojis when checking for duplicates

--- src/services/twitter.service.ts
+++ src/services/twitter.service.ts
@@ -26,7 +26,7 @@ export default class TwitterService {
   }
 
   public static tweetMatch(matchData: MatchTweetData): Promise<void> {
-    const playersTweetText = this.buildMatchTweetText(matchData);
+    const playersTweetText = this.buildMatchTweetText(matchData, true);
     const linksTweetText = this.buildLinksTweetText(matchData);
 
     return this.twitterClient.v2
@@ -51,7 +51,7 @@ export default class TwitterService {
   public static async isMatchTweeted(
     matchData: MatchTweetData
   ): Promise<boolean> {
-    const searchText = this.buildMatchTweetText(matchData);
+    const searchText = this.buildMatchTweetText(matchData, false);
     const result = await this.twitterClient.v2.search(
       `"${searchText}" (from:${Config.TWITTER_USERNAME})`
     ); // twitter throws error on duplicate content too "You are not allowed to create a Tweet with duplicate content."
@@ -59,23 +59,26 @@ export default class TwitterService {
     return result.data.meta.result_count > 0;
   }
 
-  private static buildMatchTweetText(matchData: MatchTweetData): string {
+  private static buildMatchTweetText(
+    matchData: MatchTweetData,
+    includeLink: boolean
+  ): string {
     const { blueTeam, redTeam } = matchData.match;
     let text = "";
 
-    text += `TOP 🏔 ${this.buildPlayerText(blueTeam[0])}\n`;
-    text += `JGL 🌲 ${this.buildPlayerText(blueTeam[1])}\n`;
-    text += `MID 🪄 ${this.buildPlayerText(blueTeam[2])}\n`;
-    text += `BOT 🏹 ${this.buildPlayerText(blueTeam[3])}\n`;
-    text += `SUP 🛡 ${this.buildPlayerText(blueTeam[4])}\n\n`;
+    text += `TOP 🏔 ${this.buildPlayerText(blueTeam[0], includeLink)}\n`;
+    text += `JGL 🌲 ${this.buildPlayerText(blueTeam[1], includeLink)}\n`;
+    text += `MID 🪄 ${this.buildPlayerText(blueTeam[2], includeLink)}\n`;
+    text += `BOT 🏹 ${this.buildPlayerText(blueTeam[3], includeLink)}\n`;
+    text += `SUP 🛡 ${this.buildPlayerText(blueTeam[4], includeLink)}\n\n`;
 
     text += "vs\n\n";
 
-    text += `TOP 🏔 ${this.buildPlayerText(redTeam[0])}\n`;
-    text += `JGL 🌲 ${this.buildPlayerText(redTeam[1])}\n`;
-    text += `MID 🪄 ${this.buildPlayerText(redTeam[2])}\n`;
-    text += `BOT 🏹 ${this.buildPlayerText(redTeam[3])}\n`;
-    text += `SUP 🛡 ${this.buildPlayerText(redTeam[4])}`;
+    text += `TOP 🏔 ${this.buildPlayerText(redTeam[0], includeLink)}\n`;
+    text += `JGL 🌲 ${this.buildPlayerText(redTeam[1], includeLink)}\n`;
+    text += `MID 🪄 ${this.buildPlayerText(redTeam[2], includeLink)}\n`;
+    text += `BOT 🏹 ${this.buildPlayerText(redTeam[3], includeLink)}\n`;
+    text += `SUP 🛡 ${this.buildPlayerText(redTeam[4], includeLink)}`;
 
     // text += `Patch: ${ChampsQueueService.CQ_CURR_PATCH}\n`;
 
@@ -84,11 +87,11 @@ export default class TwitterService {
 
   private static buildPlayerText(
     matchPlayer: MatchPlayer,
-    includeLinks: boolean = true
+    includeLink: boolean // needed to support twitter search (might be checking a channel that just went live)
   ): string {
     let text = matchPlayer.summonerNameWithTeam;
 
-    if (matchPlayer.isStreaming && includeLinks) {
+    if (matchPlayer.isStreaming && includeLink) {
       text += ` 📺`;
     }
 
