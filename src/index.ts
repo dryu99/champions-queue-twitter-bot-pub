2559cd4ce6a4776d99c707ad5d769deec93e01f4
Daniel Ryu <rujjiryu99@gmail.com>
2022-09-01 14:02:19 -0700

    clean up some comments

--- src/index.ts
+++ src/index.ts
@@ -17,73 +17,13 @@ Server.start().catch((error) => {
   return BugService.close(2000).then(() => process.exit(1));
 });
 
-// const matchData: MatchTweetData = {
-//   match: {
-//     blueTeam: [
-//       {
-//         summonerNameWithTeam: "100 aaaaaaaa",
-//         isStreaming: false,
-//         twitchUsername: "a",
-//       },
-//       {
-//         summonerNameWithTeam: "TSM bbbbbbbb",
-//         isStreaming: true,
-//         twitchUsername: "bbbbbbbbbbbbb",
-//       },
-//       {
-//         summonerNameWithTeam: "TSM ccccc",
-//         isStreaming: false,
-//         twitchUsername: "c",
-//       },
-//       {
-//         summonerNameWithTeam: "CLG dddddddddd",
-//         isStreaming: true,
-//         twitchUsername: "dddd",
-//       },
-//       {
-//         summonerNameWithTeam: "100 eeee",
-//         isStreaming: false,
-//         twitchUsername: "e",
-//       },
-//     ],
-//     redTeam: [
-//       {
-//         summonerNameWithTeam: "Lourlo",
-//         isStreaming: true,
-//         twitchUsername: "lourlo",
-//       },
-//       {
-//         summonerNameWithTeam: "FLY bbbbbbbbbb",
-//         isStreaming: false,
-//         twitchUsername: "FLY b",
-//       },
-//       {
-//         summonerNameWithTeam: "FLY ccccccccc",
-//         isStreaming: false,
-//         twitchUsername: "FLY c",
-//       },
-//       {
-//         summonerNameWithTeam: "FLY ddddddddd",
-//         isStreaming: false,
-//         twitchUsername: "FLY d",
-//       },
-//       {
-//         summonerNameWithTeam: "FLY eeeeeeeee",
-//         isStreaming: false,
-//         twitchUsername: "FLY e",
-//       },
-//     ],
-//   },
-//   author: "testuser",
-// };
-
-// setup
+// Setup
 // - store all player metadata in db (player metadata should be updated periodically as new players join queue)
 //   - twitch handle
 //   - summoner name
 //
 
-// workflow
+// Workflow
 // - listen to MAX_CHANNELS channels that are (LIVE + playing league of legends)
 //   - periodically try listening to unlive channels to see if they're live
 // - send !teams msg periodically to every channel
@@ -99,7 +39,7 @@ Server.start().catch((error) => {
 
 //
 /**
- * method 1 (send !teams command and listen to nightbot response)
+ * Method 1 (send !teams command and listen to nightbot response)
  * pros: don't have to listen to streams infinitely
  * cons: not a good way to validate !teams response (besides adding metadata to msg)
  *
@@ -115,7 +55,7 @@ Server.start().catch((error) => {
 
 //
 /**
- * method 2 (listen to all channels for mod msg)
+ * Method 2 (listen to all channels for mod msg)
  * pros: don't have to worry about outdated msgs + can work with people who don't have nightbot
  * cons: have to listen to streams for longer
  *
@@ -132,8 +72,3 @@ Server.start().catch((error) => {
  *     - post tweet!
  * - every y minutes check pendingChannels, if 20 min has passed since added, add back to list
  */
-
-// const channels = [];
-// const pendingChannels = [];
-// const liveChannels = [];
-// const ongoingGameChannels = [];
