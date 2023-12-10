a52418b00dce69225e381af6006ed29b4bb89e83
Daniel Ryu <rujjiryu99@gmail.com>
2023-12-10 21:52:38 +0900

    chore: update script for building image json

--- src/scripts/build-base64-img-json.ts
+++ src/scripts/build-base64-img-json.ts
@@ -5,31 +5,36 @@ import path from "path";
 
 console.log("generating base 64 json map");
 
-const outputFilename = "role-logos";
-const imgDirPath = `${process.cwd()}/src/ui/assets/role-logos/`;
-const imgFilePaths = fs.readdirSync(imgDirPath);
+const buildImageJson = (assetPath: string, outputFilename: string) => {
+  const imgDirPath = `${process.cwd()}${assetPath}`;
+  const imgFilePaths = fs.readdirSync(imgDirPath);
 
-const base64Map: Record<string, { base64: string }> = {};
+  const base64Map: Record<string, { base64: string }> = {};
 
-for (const imgFilePath of imgFilePaths) {
-  const key = imgFilePath.split(".")[0].toUpperCase();
-  const ext = path.extname(imgFilePath).split(".")[1];
-  const base64 = fs.readFileSync(`${imgDirPath}/${imgFilePath}`, {
-    encoding: "base64",
+  for (const imgFilePath of imgFilePaths) {
+    const key = imgFilePath.split(".")[0].toUpperCase();
+    const ext = path.extname(imgFilePath).split(".")[1];
+    const base64 = fs.readFileSync(`${imgDirPath}/${imgFilePath}`, {
+      encoding: "base64",
+    });
+
+    console.log("adding img base64", {
+      imgFilePath,
+      base64: base64.substring(0, 5),
+    });
+    base64Map[key] = { base64: `data:image/${ext};base64,${base64}` };
+  }
+
+  fs.writeFileSync(
+    `${process.cwd()}/src/ui/assets/${outputFilename}.json`,
+    JSON.stringify(base64Map)
+  );
+
+  console.log("base64 json map generation complete", {
+    files: imgFilePaths.length,
+    outputFilename,
   });
+};
 
-  console.log("adding img base64", {
-    imgFilePath,
-    base64: base64.substring(0, 5),
-  });
-  base64Map[key] = { base64: `data:image/${ext};base64,${base64}` };
-}
-
-fs.writeFileSync(
-  `${process.cwd()}/src/ui/assets/${outputFilename}.json`,
-  JSON.stringify(base64Map)
-);
-
-console.log("base64 json map generation complete", {
-  files: imgFilePaths.length,
-});
+buildImageJson("/src/ui/assets/role-logos/", "role-logos");
+buildImageJson("/src/ui/assets/team-logos/", "team-logos");
