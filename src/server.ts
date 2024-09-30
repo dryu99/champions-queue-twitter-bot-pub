2a45e37011eadfe6b95c6b8c73e2c328e3bbef78
Daniel Ryu <rujjiryu99@gmail.com>
2024-09-30 10:23:33 -0700

    feat: enable title check for channels

--- src/server.ts
+++ src/server.ts
@@ -215,7 +215,7 @@ export default class Server {
     const checkChannelPromises: Promise<void>[] = [];
     for (const channel of channels) {
       // this promise updates channel list states if the channel is live
-      const checkChannelPromise = TwitchService.isChannelLive(channel)
+      const checkChannelPromise = TwitchService.isChannelLive(channel, true)
         .then((isChannelLive) => {
           const player = this.twitchPlayerData.get(channel)!;
           if (!isChannelLive) {
